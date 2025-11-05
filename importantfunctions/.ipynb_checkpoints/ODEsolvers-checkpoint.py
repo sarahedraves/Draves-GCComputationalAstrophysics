b482f1a59a71970dbd504d96ac31a19cb68e94b0
Sarah Draves <sarah@draves.org>
2025-11-05 17:57:47 -0500

    fixing all ODE solver code because the time steps were one off. still can't get verlet quite right.

--- importantfunctions/.ipynb_checkpoints/ODEsolvers-checkpoint.py
+++ importantfunctions/.ipynb_checkpoints/ODEsolvers-checkpoint.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-def Euler(diffeq, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make this generalizable so the necessary arguments can vary based on the parameters for diffeq
+def Euler(f, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make this generalizable so the necessary arguments can vary based on the parameters for diffeq
     if type(r0s)!=np.ndarray: #so this still works for only one equation instead of multiple ODEs
         r0s=np.array([r0s])
     if (N is None and maxt is None) or (N is not None and maxt is not None):
@@ -8,18 +8,18 @@ def Euler(diffeq, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make 
         return None,None
 
     #initialize arrays
-    if N is not None: #go N steps
-        Ts=np.linspace(t0,N*h,N)
+    if N is not None: #go N steps so need N+1 points
+        Ts=np.linspace(t0,t0+N*h,N+1)
     elif maxt is not None: #go to maxt
         Ts=np.arange(t0,maxt+h,h)
     Rs=np.zeros((Ts.size,r0s.size))
     Rs[0,:]=r0s
 
-    for i, t in enumerate(Ts[1:],start=1): #start at second point
-        Rs[i]=Rs[i-1]+h*diffeq(Rs[i-1],t, *args)
+    for i in range(1, len(Ts)): #starting at the second point
+        Rs[i]=Rs[i-1]+h*f(Rs[i-1],Ts[i-1], *args)
     return Ts, Rs
 
-def RK4(diffeq, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make this generalizable so the necessary arguments can vary based on the parameters for diffeq
+def RK4(f, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make this generalizable so the necessary arguments can vary based on the parameters for diffeq
     if type(r0s)!=np.ndarray: #so this still works for only one equation instead of multiple ODEs
         r0s=np.array([r0s])
     if (N is None and maxt is None) or (N is not None and maxt is not None):
@@ -27,17 +27,19 @@ def RK4(diffeq, r0s, *args, t0=0, h=0.1, N=None, maxt=None): #args is to make th
         return None,None
 
     #initialize arrays
-    if N is not None: #go N steps
-        Ts=np.linspace(t0,N*h,N)
+    if N is not None: #go N steps so need N+1 points
+        Ts=np.linspace(t0,t0+N*h,N+1)
     elif maxt is not None: #go to maxt
         Ts=np.arange(t0,maxt+h,h)
     Rs=np.zeros((Ts.size,r0s.size))
     Rs[0,:]=r0s
 
-    for i, t in enumerate(Ts[1:],start=1): #start at second point
-        k1=h*diffeq(Rs[i-1],t, *args)
-        k2=h*diffeq(Rs[i-1]+0.5*k1,t+0.5*h, *args)
-        k3=h*diffeq(Rs[i-1]+0.5*k2,t+0.5*h, *args)
-        k4=h*diffeq(Rs[i-1]+k3,t+h, *args)
-        Rs[i]=Rs[i-1]+(1/6)*(k1+2*k2+2*k3+k4) 
+    for i in range(1, len(Ts)): #starting at the second point
+        t=Ts[i-1]
+        r=Rs[i-1]
+        k1=h*f(r,t, *args)
+        k2=h*f(r+0.5*k1,t+0.5*h, *args)
+        k3=h*f(r+0.5*k2,t+0.5*h, *args)
+        k4=h*f(r+k3,t+h, *args)
+        Rs[i]=r+(1/6)*(k1+2*k2+2*k3+k4)
     return Ts, Rs
